faffa42e4ec826cfca73bc1d97ab3c57137580a6
KurtAg1 <kurtagius613@gamil.com>
2020-07-27 17:10:06 +0200

    adding files

---
+++ script.js
@@ -0,0 +1,48 @@
+const button = document.getElementById("button");
+const audioElement = document.getElementById("audio");
+
+// Disable / Enable Button
+function toggleButton() {
+  button.disabled = !button.disabled;
+}
+
+// Passing Joke to VoiceRSS API
+function tellMe(joke) {
+  VoiceRSS.speech({
+    key: "e35fe5139a2b4987907af0b1172bea11",
+    src: joke,
+    hl: "en-us",
+    v: "Linda",
+    r: 0,
+    c: "mp3",
+    f: "44khz_16bit_stereo",
+    ssml: false,
+  });
+}
+
+// Get Jokes from Joke API
+async function getJokes() {
+  let joke = "";
+  const apiUrl = "https://sv443.net/jokeapi/v2/joke/Programming";
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    if (data.setup) {
+      // Two part joke
+      joke = `${data.setup} ... ${data.delivery}`;
+    } else {
+      joke = data.joke;
+    }
+    // Text-to-Speech
+    tellMe(joke);
+    // Disable Button
+    toggleButton();
+  } catch (error) {
+    // Catch Errors Here
+    console.log("whoops", error);
+  }
+}
+
+// Event Listeners
+button.addEventListener("click", getJokes);
+audioElement.addEventListener("ended", toggleButton);
